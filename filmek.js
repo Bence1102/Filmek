15ccdd95ba1219fc374f19fd95e39f0063d13746
Bence1102 <szente.bence04@gmail.com>
2025-08-19 10:46:53 +0200

    kezdes

---
+++ filmek.js
@@ -0,0 +1,82 @@
+export const FilmLista = [
+    {
+        cim: 'A keresztapa',
+        kep: 'kepek/keresztapa.jpg',
+        leiras: 'Francis Ford Coppola klasszikusa, a maffiafilmek alapköve.',
+        jellemzok: [
+            '1972-ben készült',
+            'Oscar-díjas',
+            'Al Pacino főszerepben',
+            'Maffia történet',
+            'Ikonikus idézetek'
+        ],
+        ar: "3 500 Ft",
+        kedvenc: false,
+        kategoria: 'Dráma',
+        link: 'https://www.imdb.com/title/tt0068646/'
+    },
+    {
+        cim: 'A Gyűrűk Ura: A Gyűrű Szövetsége',
+        kep: 'kepek/lotr1.jpg',
+        leiras: 'Epikus fantasy kaland, ahol hobbitok, emberek és tündék szállnak szembe a Sötét Úrral.',
+        jellemzok: [
+            '2001-ben készült',
+            'J.R.R. Tolkien regénye alapján',
+            'Oscar-díjas',
+            'Peter Jackson rendezésében',
+            'Fantasy eposz'
+        ],
+        ar: "4 200 Ft",
+        kedvenc: false,
+        kategoria: 'Fantasy',
+        link: 'https://www.imdb.com/title/tt0120737/'
+    },
+    {
+        cim: 'Eredet',
+        kep: 'kepek/inception.jpg',
+        leiras: 'Christopher Nolan sci-fi thrillere az álomról és valóságról.',
+        jellemzok: [
+            '2010-ben készült',
+            'Leonardo DiCaprio főszerepben',
+            'Akció és pszichológia',
+            'Látványos álomvilágok',
+            'Nolan-féle időkezelés'
+        ],
+        ar: "3 800 Ft",
+        kedvenc: false,
+        kategoria: 'Sci-Fi',
+        link: 'https://www.imdb.com/title/tt1375666/'
+    },
+    {
+        cim: 'Forrest Gump',
+        kep: 'kepek/forrestgump.jpg',
+        leiras: 'Tom Hanks alakítása egy különleges férfi életútjáról.',
+        jellemzok: [
+            '1994-ben készült',
+            'Oscar-díjas',
+            'Inspiráló történet',
+            'Romantikus és drámai elemek',
+            'Időutazás az amerikai történelemben'
+        ],
+        ar: "3 000 Ft",
+        kedvenc: false,
+        kategoria: 'Dráma',
+        link: 'https://www.imdb.com/title/tt0109830/'
+    },
+    {
+        cim: 'A sötét lovag',
+        kep: 'kepek/darkknight.jpg',
+        leiras: 'Batman legendás összecsapása Jokerrel.',
+        jellemzok: [
+            '2008-ban készült',
+            'Heath Ledger Oscar-díjas alakítása',
+            'Christopher Nolan rendezte',
+            'Akció és pszichológia',
+            'Kultikus szuperhős film'
+        ],
+        ar: "4 500 Ft",
+        kedvenc: false,
+        kategoria: 'Akció',
+        link: 'https://www.imdb.com/title/tt0468569/'
+    }
+];
